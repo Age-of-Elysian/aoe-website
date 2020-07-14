a6ad18610e869fab548a55d19665f9add9d73172
BuildTools <unconfigured@null.spigotmc.org>
2020-07-14 14:49:50 +0300

    21by9 dynmap + text

--- src/Dynmap.svelte
+++ src/Dynmap.svelte
@@ -1,3 +1,17 @@
-<div class="embed-responsive embed-responsive-16by9 dynmap">
+<div class="embed-responsive embed-responsive-21by9 mb-4">
   <iframe src="http://104.128.60.178:8128/#" title="Dynmap" />
+</div>
+<div class="container text-light ">
+  <div class="text-center mb-4">
+    <h2>Our Dynmap</h2>
+  </div>
+  <p class="text-justify">
+    Dynmap is a plugin that creates a Google Maps-like map for your minecraft
+    server, which can be viewed in a browser. Players and towns are visible on
+    the map, and you can even chat using it. Your messages from the dynmap web
+    chat will also be transfered to the discord #ingamechat channel. Dynmap is
+    quite useful for exploration and for meeting up with other players. It"s
+    also a great way of checking out towns or builds by using the isometric
+    rendering mode.
+  </p>
 </div>
